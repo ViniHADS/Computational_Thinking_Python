928e0382514a9788ac871f8a6e4092c5daf65bbf
Vinicius Araujo <90992850+ViniHADS@users.noreply.github.com>
2026-04-21 15:22:30 -0300

    Organização do REP
    Organizei as pastas de aulas e adicionei uma pasta de exercício e adicionei um exercicio de revisão sobre listas.

--- Aula10/teste_numpy.py
+++ Aula10/teste_numpy.py
@@ -1,4 +1,13 @@
+# -*- coding: utf-8 -*-
 import numpy as np
 
 print("NumPy instalado com sucesso!")
-print(f"Vers�o: {np.__version__}")
+print(f"Versao: {np.__version__}")
+
+#   usando numpy, ja tendo uma variavel de nota como faco uma variavel de media?
+#   Para calcular a media usando NumPy, voce pode usar a funcao `np.mean()`. Suponha que voce tenha
+#   uma variavel de notas chamada `notas`, que e um array NumPy. Voce pode calcular a media da seguinte maneira:
+
+notas = np.array([85, 90, 78, 92, 88])  # Exemplo de array de notas
+media = np.mean(notas)  # Calcula a media das notas
+print(f"A media das notas e: {media}")
