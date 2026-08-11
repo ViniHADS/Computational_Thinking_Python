4606b98cf7f9e928e7a3c0ed2041088536d28729
Vinicius Araujo <90992850+ViniHADS@users.noreply.github.com>
2026-08-10 22:33:44 -0300

    Aula 2
    Activities made it on file .ipynb(Jupyter)

---
+++ Aula 2º - 2º Semestre/Teste.py
@@ -0,0 +1,5 @@
+with open ('mytest.txt', "w", encoding="utf-8") as arquivo:
+    arquivo.write("Olá! Este é um teste pessoal para saber como criar um arquivo txt com Python.\n")
+    arquivo.write("Segunda linha do arquivo começa aqui...")
+
+    
